86db1fcc1edc8351aea40e2c7093188c237c0299
Yoshi <usyoshua@gmail.com>
2022-08-08 00:03:04 -0400

    view books in db function

--- app.py
+++ app.py
@@ -105,6 +105,9 @@ def app():
             time.sleep(1.5)
         elif choice == '2':
             #view books
+            for book in session.query(Book):
+                print(f'{book.id} | {book.title} | {book.author}')
+            input('\nPress enter to return to the main menu.')
             pass
         elif choice == '3':
             #search for a book
